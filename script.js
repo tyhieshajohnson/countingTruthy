bc0fbab1b149d510e2d5a8bf0c88db2b3e94aecc
tyhiesha johnson <140961082+tyhieshajohnson@users.noreply.github.com>
2023-11-27 14:26:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Create an array of boolean values
+        let booleanArray = [true, false, true, true, false, true, false, false, true];
+
+        // Count true values in the array
+        let trueCount = 0;
+
+        // Loop through the array
+        for (var i = 0; i < booleanArray.length; i++) {
+            if (booleanArray[i]) {
+                trueCount++;
+            }
+        }
+
+        // Display the count of true values
+        let resultSection = document.getElementById('result');
+        resultSection.innerHTML = "<p>Count of true values: " + trueCount + "</p>";
+
+// had to get ai help with this project because it was really hard...
